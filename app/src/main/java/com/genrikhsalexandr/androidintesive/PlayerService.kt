fc8235be4c2df2629ebe19e828d0938ad053f02f
aleksandr.genrikhs <aleksandr.genrikhs@gmail.com>
2023-09-21 23:49:52 +0300

    fix notification

--- app/src/main/java/com/genrikhsalexandr/androidintesive/PlayerService.kt
+++ app/src/main/java/com/genrikhsalexandr/androidintesive/PlayerService.kt
@@ -1,16 +1,20 @@
 package com.genrikhsalexandr.androidintesive
 
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 
 class PlayerService : LifecycleService() {
     companion object {
         private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
         private const val NOTIFICATION_ID = 1
 
         fun startService(context: Context) {
@@ -18,26 +22,34 @@ class PlayerService : LifecycleService() {
             context.startForegroundService(startIntent)
             Log.d("PlayerService", "startService")
         }
-
     }
+
     override fun onCreate() {
         super.onCreate()
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel =
+            NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
         Log.d("PlayerService", "onCreate")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
         val pendingIntent: PendingIntent =
-            Intent(this, PlayerService::class.java).let { notificationIntent ->
-                PendingIntent.getService(this, 0, notificationIntent,
-                    PendingIntent.FLAG_IMMUTABLE)
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(
+                    this, 0, notificationIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
             }
 
-        val notification: Notification = Notification.Builder(this, CHANNEL_ID)
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Music Player")
             .setContentText("Playing Music")
             .setSmallIcon(R.drawable.ic_music_note)
             .setContentIntent(pendingIntent)
+            .addAction(R.drawable.ic_play, "Play", pendingIntent)
             .build()
         startForeground(NOTIFICATION_ID, notification)
         Log.d("PlayerService", "onStartCommand")
@@ -55,6 +67,3 @@ class PlayerService : LifecycleService() {
         Log.d("PlayerService", "onDestroy")
     }
 }
-
-
-
