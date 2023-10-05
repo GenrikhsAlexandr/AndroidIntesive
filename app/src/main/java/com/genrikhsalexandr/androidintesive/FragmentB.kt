c58c09c696daef769598aa81a515b5cb829e0ce3
aleksandr.genrikhs <aleksandr.genrikhs@gmail.com>
2023-10-05 14:26:42 +0300

    add navigation component + Safe Args

--- app/src/main/java/com/genrikhsalexandr/androidintesive/FragmentB.kt
+++ app/src/main/java/com/genrikhsalexandr/androidintesive/FragmentB.kt
@@ -6,42 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.genrikhsalexandr.androidintesive.FragmentBDirections.Companion.actionFragmentBToFragmentC
 import com.genrikhsalexandr.androidintesive.databinding.FragmentBBinding
 
 class FragmentB : Fragment() {
 
-        private var _binding: FragmentBBinding? = null
-        private val binding: FragmentBBinding get() = _binding!!
+    private var _binding: FragmentBBinding? = null
+    private val binding: FragmentBBinding get() = _binding!!
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View {
-            _binding = FragmentBBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBBinding.inflate(inflater, container, false)
 
-            with(binding) {
-                btFragmentC.setOnClickListener {
-                    toFragmentC()
-                }
-                btBackA.setOnClickListener {
-                    toFragmentA()
-                }
+        with(binding) {
+            btFragmentC.setOnClickListener {
+                toFragmentC()
             }
-            return binding.root
+            btBackA.setOnClickListener {
+                toFragmentA()
             }
-
-
-    private fun toFragmentC(){
-        val hello = "Hello Fragment C"
-        val bundle =Bundle()
-        bundle.putString("message", hello)
-        findNavController().navigate(R.id.action_fragmentB_to_fragmentC, bundle)
+        }
+        return binding.root
     }
-    private fun toFragmentA(){
+
+
+    private fun toFragmentC() {
+        val hello = "Hello Fragment C"
+        val action = FragmentADirections.actionFragmentAToFragmentB(hello)
+        findNavController().navigate(action)
+    }
+
+    private fun toFragmentA() {
         findNavController().navigate(R.id.action_fragmentB_to_fragmentA)
     }
-        override fun onDestroyView() {
-            super.onDestroyView()
-            _binding = null
-        }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/genrikhsalexandr/androidintesive/FragmentC.kt
+++ app/src/main/java/com/genrikhsalexandr/androidintesive/FragmentC.kt
@@ -6,21 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.genrikhsalexandr.androidintesive.databinding.FragmentCBinding
 
 class FragmentC : Fragment() {
 
     private var _binding: FragmentCBinding? = null
     private val binding: FragmentCBinding get() = _binding!!
+    private val args:FragmentBArgs by navArgs()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCBinding.inflate(inflater, container, false)
-        val message = arguments?.getString("message")
         with(binding) {
-            tvHello.text = message
+            tvHello.text = args.hello
             btFragmentD.setOnClickListener {
                 toFragmentD()
             }
